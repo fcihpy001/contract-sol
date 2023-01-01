c99db0845e281c200d00a4a84aadd12028c4beb9
fcihpy <33910310@qq.com>
2023-01-01 20:21:48 +0800

    新增xen-token

---
+++ XEN/interface/IStakingToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IStakingToken {
+    event Staked(address indexed user, uint256 amount, uint256 term);
+    event Withdraw(address indexed user, uint256 amount, uint256 reward);
+    function stake (uint256 amount, uint256 term) external;
+    function withdraw() external;
+    
+}
